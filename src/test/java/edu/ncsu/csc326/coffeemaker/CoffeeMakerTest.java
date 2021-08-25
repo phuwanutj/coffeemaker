a0dd3041b48bfd4a159c697a399399729ed2859e
unknown <phuwanut.j@ku.th>
2021-08-25 14:14:41 +0700

    Edit documentation

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -29,7 +29,7 @@ import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 /**
  * Unit tests for CoffeeMaker class.
  * 
- * @author Sarah Heckman
+ * @author Phuwanut Jiamwatthanaloet
  */
 public class CoffeeMakerTest {
     
